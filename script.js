5179535f782dae5236d88bffd850ddf73829d1fa
jossnock <19121867@hlnsc.ac.uk>
2024-06-28 15:46:54 +0100

    added twhioaef

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
-function convertAndReplace(id) {
-    var money;
-    money = parseFloat(document.getElementById(id).value);
 
-    if (id == "GBPs") {
-        money *= 1.18;
-        document.getElementById("EURs").value = money.toFixed(2);
-    }
 
-    else if (id == "EURs") {
-        money /= 1.18;
-        document.getElementById("GBPs").value = money.toFixed(2);
-    }
+var currencyData = [] //Global variable to house currency data. Inspect it in the console after the page has loaded.
+
+/**
+ * The function `populateCurrencyDataAsync` asynchronously fetches currency data from an API and stores
+ * it in a variable.
+ */
+async function populateCurrencyDataAsync() {
+  try {
+    const response = await fetch('https://openexchangerates.org/api/latest.json?app_id=e64435a2bdd146fb83869a4263fe645f');
+    const data = await response.json();
+    currencyData = data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function getExchangeRate(from, to, data = currencyData){
+    let rate = data['rates'][to] / data['rates'][from];
+    document.getElementById("exRt").value = "Exchange rate: 1 GBP = ${rate} EUR";
+}
+
+
+/**
+ * The function `convert` takes a value in GBP, converts it to euros using a fixed exchange rate of
+ * 1.18, and displays the result in a designated input field.
+ * @param from - A 3 character upper-case string international currency code, e.g. 'GBP' to represent the original currency.
+ * @param to - A 3 character upper-case string international currency code, e.g. 'EUR' to represent the desired currency.
+ */
+function convertAndReplace(from, to, data = currencyData){
+  let rate = data['rates'][to] / data['rates'][from];
+  console.log(rate);
+
+  var price = document.getElementById(from).value * rate;
+  console.log(price);
+  
+  document.getElementById(to).value = Number(price.toFixed(4));
 }
